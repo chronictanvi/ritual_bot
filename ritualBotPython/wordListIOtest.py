f7a50f41890ed06b8ccd287b2ca92b4fef61720c
Peter <pcturnbull7@gmail.com>
2019-09-18 16:11:27 -0400

    Added multiple origin functionality

--- ritualBotPython/wordListIOtest.py
+++ ritualBotPython/wordListIOtest.py
@@ -11,14 +11,23 @@ print("Establishing Adafruit IO client...")
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 print("...done.")
 
+# Sophie's feeds test?
+#IO_FEED = 'sharingtest.feedtest'
+#IO_FEED_USERNAME = 'perhapsfrogs'
+
 print("Getting word data from Adafruit IO...")
 # Get an array of all data from feed 'Test'
 adjectivesData = aio.data('ritualbot.adjectives')
 placesData = aio.data('ritualbot.placewords')
 verbsData = aio.data('ritualbot.verbs')
+pastVerbsData = aio.data('ritualbot.pastverbs')
+occurrencesData = aio.data('ritualbot.occurrences')
 nounsData = aio.data('ritualbot.nouns')
 adverbsData = aio.data('ritualbot.adverbs')
 movementVerbsData = aio.data('ritualbot.movementverbs')
+
+# testing getting data from Sophie's feeds?
+# sophieData = aio.data('sharingtest.feedtest')
 print("...done.")
 
 print("Appending word data to arrays...")
@@ -38,6 +47,16 @@ for d in verbsData:
     #print('Data value: {0}'.format(d.value))
     verbs.append(d.value)
 
+pastVerbs = []
+for d in pastVerbsData:
+    #print('Data value: {0}'.format(d.value))
+    pastVerbs.append(d.value)
+
+occurrences = []
+for d in occurrencesData:
+    #print('Data value: {0}'.format(d.value))
+    occurrences.append(d.value)
+
 nouns = []
 for d in nounsData:
     #print('Data value: {0}'.format(d.value))
@@ -52,14 +71,19 @@ moveVerbs = []
 for d in movementVerbsData:
     #print('Data value: {0}'.format(d.value))
     moveVerbs.append(d.value)
+
+# sophieTest = []
+# for d in sophieData:
+#     print('Data value: {0}'.format(d.value))
+#     sophieTest.append(d.value)
 print("...done.")
 
-lineWidth = 16
+lineWidth = 32
 
 def break_text(string):
     print(len(string))
-    if(len(string) >= 16):
-        index = string.rfind(" ", 0, 16)
+    if(len(string) >= lineWidth):
+        index = string.rfind(" ", 0, lineWidth)
         #string = string[:index] + "\n" + string[(index+1):]
         string1, string2 = string[:index], string[index+1:]
         print(len(string1))
@@ -69,11 +93,47 @@ def break_text(string):
         print(string)
         #printer.write(l + "\n" + r1 + "\n" + r2)
 
-# wake up in your (adjective) (place). (verb) your (noun) (adverb). (moveVerb) to your (place).
-line1 = (f"wake up in your {random.choice(adjectives)} {random.choice(places)}.")
-line2 = (f"{random.choice(verbs)} your {random.choice(nouns)} {random.choice(adverbs)}.")
-line3 = (f"{random.choice(moveVerbs)} to your {random.choice(places)}.")
+def generate_ritual():
+    origin = random.randint(1,3)
+    print("Origin {} selected.".format(origin))
+    if(origin == 1):
+        line1 = (f"wake up in your {random.choice(adjectives)} {random.choice(places)}.")
+        line2 = (f"{random.choice(verbs)} your {random.choice(nouns)} {random.choice(adverbs)}.")
+        line3 = (f"{random.choice(moveVerbs)} to your {random.choice(places)}.")
 
-break_text(line1)
-break_text(line2)
-break_text(line3)
+        break_text(line1)
+        break_text(line2)
+        break_text(line3)
+    elif(origin == 2):
+        line1 = (f"you are {random.choice(adjectives)} again.")
+        line2 = (f"{random.choice(adverbs)}, {random.choice(verbs)} your {random.choice(adjectives)} self and {random.choice(verbs)} from the {random.choice(nouns)}.")
+        line3 = (f"bearing {random.choice(nouns)} of {random.choice(nouns)} on which a {random.choice(nouns)} and a {random.choice(nouns)} {random.choice(verbs)} {random.choice(adverbs)}.")
+
+        break_text(line1)
+        break_text(line2)
+        break_text(line3)
+    elif(origin == 3):
+        line1 = (f"do you remember the first time you {random.choice(pastVerbs)} your {random.choice(nouns)}?")
+        line2 = (f"if only you could {random.choice(verbs)} {random.choice(occurrences)}, you might {random.choice(verbs)} something for the {random.choice(nouns)}.")
+
+        break_text(line1)
+        break_text(line2)
+    else:
+        print("Dunno what happened dude my bad")
+
+
+# wake up in your (adjective) (place). (verb) your (noun) (adverb). (moveVerb) to your (place).
+#line1 = (f"wake up in your {random.choice(adjectives)} {random.choice(places)}.")
+#line2 = (f"{random.choice(verbs)} your {random.choice(nouns)} {random.choice(adverbs)}.")
+#line3 = (f"{random.choice(moveVerbs)} to your {random.choice(places)}.")
+
+# you are (adjective) again. (adverb), (verb) your (adjective) self and (verb) from the (noun). bearing  (noun) of (noun) on which a (noun) and a (noun) (verb) (adverb).
+#line1 = (f"you are {random.choice(adjectives)} again.")
+#line2 = (f"{random.choice(adverbs)}, {random.choice(verbs)} your {random.choice(adjectives)} self and {random.choice(verbs)} from the {random.choice(nouns)}.")
+#line3 = (f"bearing {random.choice(nouns)} of {random.choice(nouns)} on which a {random.choice(nouns)} and a {random.choice(nouns)} {random.choice(verbs)} {random.choice(adverbs)}.")
+
+# do you remember the first time you (pastverb) your (noun)? if only you could (verb) (occurrence), you might (verb) something for the (noun).
+#line1 = (f"do you remember the first time you {random.choice(pastVerbs)} your {random.choice(nouns)}?")
+#line2 = (f"if only you could {random.choice(verbs)} {random.choice(occurrences)}, you might {random.choice(verbs)} something for the {random.choice(nouns)}.")
+
+generate_ritual()
